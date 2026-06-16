45ab0cfccba7f94069abe3b7e10b9abb97612f8c
saikruopaCPQA <saikrupa.j@copperpoddigital.com>
2026-06-16 15:22:15 +0530

    Initial commit

--- Demo.js
+++ Demo.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!OPO");
+let num = 10;
+const name = "sai";
+const boolean = true;
+const age = 25;
+const score = 85;
+score= 90   
+console.log(num, name, boolean, age, score);
